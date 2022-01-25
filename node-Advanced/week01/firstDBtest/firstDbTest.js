bf91544145e5e83973bf035dd308baf209f8b0dd
Ouss84 <bahrioussama84@gmail.com>
2022-01-25 10:57:17 +0200

    select, insert, delete and update the db using js scripts

--- node-Advanced/week01/firstDBtest/firstDbTest.js
+++ node-Advanced/week01/firstDBtest/firstDbTest.js
@@ -9,25 +9,53 @@ async function testA() {
     user: "zeke",
     password: "secret",
     database: "employeedb",
-    allowPublicKeyRetrieval: true,
+    allowPublicKeyRetrieval: true, //mysql users add this
   });
-  console.log("######## Test 1 ########");
+  console.log("####### Test 1 ########");
   let result = await connection.query("select * from employee");
   console.log(result);
-
+  console.log("******** meta deleted *********");
   delete result.meta;
   console.log(result);
+
   for (let person of result) {
-    console.log(`${person.firstname}:${person.salary}€`);
+    console.log(`${person.firstname}: ${person.salary} €`);
   }
-  console.log("############ Test 2 ###########");
+
+  console.log("######### test 2 ########");
   result = await connection.query(
     "select firstname,lastname from employee where employeeId=?",
     [1]
+    // "select firstname,lastname from employee where employeeId in(?,?)",
+    // [1,2]
   );
-
   delete result.meta;
   console.log(result);
+
+  console.log("############ test 3 insert ###########");
+  result = await connection.query("insert into employee values(?,?,?,?,?)", [
+    5,
+    "Bill",
+    "Bond",
+    "secret",
+    9999,
+  ]);
+  console.log(result);
+
+  // console.log("####### test 4 delete #######");
+  // result = await connection.query("delete from employee where employeeID=?", [
+  //   5,
+  // ]);
+  // console.log(result);
+
+  console.log("####### test 4 update #######");
+  result = await connection.query(
+    "update employee set department=?, salary=? where employeeId=?",
+    ["admin", 7000, 2]
+  );
+  // to db engine:update employee set department='admin', salary=7000 where employeeId=2
+  console.log(result);
+
   connection.end();
 }
 
